573db1ba7160c63c4ddb22ccf7f65d55d479b4d6
parkysood <parkysood@gmail.com>
2025-06-04 15:55:59 -0700

    Changed colors to match app's pallete

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 
-sns.set_theme(style='whitegrid')
+sns.set_theme(style="whitegrid")
 
-df = pd.read_csv('cs188-survey-data.tsv', sep='\t')
+df = pd.read_csv("cs188-survey-data.tsv", sep="\t")
 
 responses = df.columns[3:22]
 filtered_responses = [col for col in responses if "you're paying attention" not in col]
 study_habits = df[filtered_responses]
 
-fig, axes = plt.subplots(6, 3,figsize=(12,10))
+fig, axes = plt.subplots(6, 3, figsize=(12, 10))
 axes = axes.ravel()
 
 for i, column in enumerate(study_habits.columns):
     counts = study_habits[column].value_counts().sort_index()
-    
-    title = re.search(r'\[(.*?)\]', column).group(1)
-    
-    sns.barplot(x=counts.index, y=counts.values, ax=axes[i], hue=counts.index, palette='viridis', legend=False)
+
+    title = re.search(r"\[(.*?)\]", column).group(1)
+
+    sns.barplot(
+        x=counts.index,
+        y=counts.values,
+        ax=axes[i],
+        hue=counts.index,
+        palette=["#2E1065", "#4C1D95", "#7C3AED", "#A855F7", "#C084FC"],
+        legend=False,
+    )
     axes[i].set_title(title, wrap=True, fontsize=8)
     axes[i].set_xlabel("Response (1-5)", fontsize=8)
     axes[i].set_ylabel("Count", fontsize=8)
@@ -27,8 +34,6 @@ for i, column in enumerate(study_habits.columns):
 
 for j in range(len(study_habits.columns), len(axes)):
     fig.delaxes(axes[j])
-    
+
 plt.tight_layout()
 plt.show()
-
-
